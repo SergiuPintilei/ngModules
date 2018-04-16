5c80062d4b82b49ed0032cf1a68aae8841931e51
Sergiu Pintilei <sergiu.pintilei@strongbytes.ro>
2018-04-16 22:35:21 +0300

    all in one module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,41 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AdminComponent } from './admin/admin.component';
+import { UserService } from './user.service';
+import { UppercasePipe } from './uppercase.pipe';
+import { ToggleDirective } from './toggle.directive';
+import { AuthGuard } from './auth.guard';
+import { LoadingSpinnerComponent } from './loading-spinner/loading-spinner.component';
 
+const routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  { path: 'home', component: HomeComponent },
+  { path: 'admin', component: AdminComponent },
+  { path: '**', pathMatch: 'full', component: HomeComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AdminComponent,
+    UppercasePipe,
+    ToggleDirective,
+    LoadingSpinnerComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
-  providers: [],
+  providers: [
+    UserService,
+    AuthGuard
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
